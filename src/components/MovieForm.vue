922cf4d374d79fdb75d96f9e21e21d9aa5327058
superadmin <ronan.marzin@gmail.com>
2018-08-24 13:43:27 +0200

    added MovieFom.vue

---
+++ src/components/MovieForm.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <form @submit.prevent="submit">
+      <p>Movie Title</p>
+      <input type="text" v-model="title" placeholder="Movie title">
+      <p>Movie Poster</p>
+      <input type="text" v-model="poster" placeholder="Movie poster">
+      <p>Image alternative text</p>
+      <input type="text" v-model="alt" placeholder="Alternative text">
+      <p>Movie link</p>
+      <input type="text" v-model="link" placeholder="Movie link">
+      <p>Synopsis</p>
+      <input id="synopsis" type="text" v-model="synopsis" placeholder="Synopsis">
+      <div v-if="errorMessages" class="errors">
+        <p v-for="(msg, index) in errorMessages" :key="index">{{ msg }}</p>
+      </div>
+      <br>
+      <input type="submit" value="Submit"/>
+    </form>
+  </div>
+</template>
+
+<script>
+import { moviesState } from '../states/movies-state'
+export default {
+  name: 'MovieForm',
+  data () {
+    return {
+      title: '',
+      poster: '',
+      alt: '',
+      link:'',
+      synopsis:'',
+      id:0,
+      errorMessages: null
+    }
+  },
+  methods: {
+    submit () {
+/*       console.log('title', this.title)
+      console.log('link', this.link)
+      console.log('synopsis', this.synopsis) */
+      this.errorMessages = null
+      this.sendForm()
+    },
+    async sendForm () {
+      try {
+        let response = await fetch('http://localhost:5000/MovieForm', {
+          method: 'post',
+          headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          },
+          body: JSON.stringify ({
+            title: this.title,
+            poster: this.poster,
+            alt: this.alt,
+            link: this.link,
+            synopsis: this.synopsis,
+            id:0
+          })
+        })
+        if (!response.ok) {
+          if (response.status == 400) {
+            this.errorMessages = await response.json()
+            }
+            else {
+              this.errorMessages = ['unexpected error, try again']
+            }
+        } else { this.$router.push('/')}
+        
+      } catch (error) {
+      this.errorMessages = [error.message]
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+div {
+  //display: flex;
+  //flex-grow: 1;
+  //flex-direction: column;
+  //width: min-content;
+  //position: absolute;
+  // top:0;
+  // bottom: 0;
+  // left: 0;
+  // right: 0;
+  // margin: auto;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+&.errors {
+  background-color: red;
+  width:fit-content;
+  padding-left: 1px;
+  padding-right: 1px;
+  padding-top : 5px;
+  padding-bottom: 5px;
+  
+  }
+input[type=text] {
+  padding:5px;
+  border:2px solid #ccc;
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+}
+input[type=text]:focus {border-color:rgb(23, 73, 211) ; }
+
+input[type=submit] {
+  padding:5px 15px;
+  background:#ccc;
+  border:0 none;
+  cursor:pointer;
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+  margin: 5px 15px;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  }
+}
+</style>
+
